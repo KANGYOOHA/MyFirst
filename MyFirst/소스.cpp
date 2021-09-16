17e1d65a15d8ebcedb2527ae7b2401fd22e5237b
KANGYOOHA <wouiomk@daum.net>
2021-09-16 15:04:46 +0900

    더하기

--- MyFirst/소스.cpp
+++ MyFirst/소스.cpp
@@ -3,12 +3,10 @@
 using namespace std;
 
 int main() {
- 
-	cout<< "*\n";
-	cout << "**\n";
-	cout << "***\n";
-	cout << "****\n";
-	cout << "*****\n";
+  
+	cout << 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 << endl;
+	
+	
 
 	return 0;
 
